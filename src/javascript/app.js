f1d06ef1f9eeee90631898f5c3d8d689eda2cef2
Jasn <jasn74939@gmail.com>
2025-08-14 11:53:10 -0400

    css styling and subtraction

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -7,23 +7,39 @@ const protein = document.getElementById('protein');
 const fats = document.getElementById('fats');
 const carbs = document.getElementById('carbs');
 
+const norp = document.getElementById('norp');
+
+// set addition to default
+let isAddition = true; 
+
+// on click toggle between addition and subtraction
+norp.addEventListener('click', () => {
+  isAddition = !isAddition;
+  norp.textContent = isAddition ? '+ | -' : '- | +'; 
+});
+
 // Set default numbers
 let proteinCount = 0;
 let fatsCount = 0;
 let carbsCount = 0;
 
-// On click, add a number to each macro
+// on click add/subtract 1 number to the counter
 pbutton.addEventListener('click', () => {
-  proteinCount += 1;
+  proteinCount += isAddition ? 1 : -1;
+  if (proteinCount < 0) proteinCount = 0; // prevent going below zero
   protein.textContent = proteinCount;
 });
 
 fbutton.addEventListener('click', () => {
-  fatsCount += 1;
+  fatsCount += isAddition ? 1 : -1;
+  if (fatsCount < 0) fatsCount = 0; // prevent going below zero
   fats.textContent = fatsCount;
 });
 
 cbutton.addEventListener('click', () => {
-  carbsCount += 1;
+  carbsCount += isAddition ? 1 : -1;
+  if (carbsCount < 0) carbsCount = 0; // prevent going below zero
   carbs.textContent = carbsCount;
 });
+
+
